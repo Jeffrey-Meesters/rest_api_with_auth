2d044bfe7bc489c4b286a285ba3878f58253e818
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-01-21 17:30:25 +0100

    Add mongoose and connect to MongoDB, successful

--- app.js
+++ app.js
@@ -3,6 +3,26 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require("mongoose");
+
+// connect mongoose to our DB which is running localy
+mongoose.connect("mongodb://localhost:27017/fsjstd-restapi", { useNewUrlParser: true });
+// Store the connection
+const db = mongoose.connection;
+
+// Check for connection errors
+db.on("error", (error) => {
+    console.log("error happened:", error)
+});
+
+// db open connection
+db.once("open", () => {
+    console.log("DB connection seccussful!")
+    // DB code goes here
+
+    // db close connection
+    db.close();
+});
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -46,5 +66,9 @@ app.set('port', process.env.PORT || 5000);
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
+  if (server.address().port === 5000) {
+    console.log(`Express server is listening on port http://localhost:${server.address().port}`);
+  } else {
+    console.log(`Express server is listening on port ${server.address().port}`);
+  }
 });
