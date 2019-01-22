c78d54c9ca2102d10e0a9920310fcf73c7e526b5
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-01-22 22:14:24 +0100

    got all routes doing what they are supposed to do

--- routes.js
+++ routes.js
@@ -5,6 +5,19 @@ const router = express.Router();
 const User = require("./models").User;
 const Course = require("./models").Course;
 
+router.param("id", (req, res, next, id) => {
+    Course.findById(id, (err, doc) => {
+        if(err) return next(err);
+        if(!doc) {
+            err = new Error("Not found");
+            err.status = 404;
+            return next(err);
+        }
+        req.course = doc;
+        return next();
+    })
+})
+
 // GET /users
 // Route for getting current user
 router.get('/users', (req, res, next) => {
@@ -19,7 +32,12 @@ router.get('/users', (req, res, next) => {
 // POST /users
 // Route for creating users
 router.post('/users', (req, res, next) => {
-    res.status(201).json({message: 'created user'});
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if(err) return next(err);
+        res.status(201);
+        res.json(user);
+    })
 });
 
 // GET /courses
@@ -29,6 +47,7 @@ router.get('/courses', (req, res, next) => {
             .sort({createdAt: -1})
             .exec((err, users) => {
                 if(err) return next(err);
+                res.status(200);
                 res.json(users);
             });
 });
@@ -36,28 +55,37 @@ router.get('/courses', (req, res, next) => {
 // GET /courses/:id
 // Route for getting a specific course
 router.get('/courses/:id', (req, res, next) => {
-    Course.findById(req.params.id, (err, doc) => {
-        if(err) return next(err);
-        res.json(doc);
-    })
+    res.json(req.course);
 });
 
 // POST /courses
 // Route for creating a course
 router.post('/courses', (req, res, next) => {
-    res.status(201).json({message: 'Create a course'});
+    const course = new Course(req.body);
+    course.save((err, course) => {
+        if(err) return next(err);
+        res.status(201);
+        res.json(course);
+    })
 });
 
 // PUT /courses/:id
 // Route for updating a specific course
 router.put('/courses/:id', (req, res, next) => {
-    res.status(204).json({message: 'Update course'});
+    req.course.update(req.body, (err, data) => {
+        if(err) next(err);
+        res.status(201);
+        res.json(data);
+    });
 });
 
 // DELETE /courses/:id
 //  Route for deleting a specific course
 router.delete('/courses/:id', (req, res, next) => {
-    res.status(204).json({message: 'delete course'});
+    req.course.remove((err) => {
+        if(err) return next(err);
+        res.sendStatus(204);
+    })
 });
 
 module.exports = router;
