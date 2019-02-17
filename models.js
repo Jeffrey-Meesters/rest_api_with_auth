29894fbddf5da827cb54677fec147c451c878815
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-02-17 20:18:43 +0100

    Finishing up the requirements for the project

--- models.js
+++ models.js
@@ -1,8 +1,10 @@
-'user string';
+'use strict';
 const mongoose = require("mongoose");
 
 
 const Schema = mongoose.Schema;
+
+// Define userSchema
 const UserSchema = new Schema({
   firstName: String,
   lastName: String,
@@ -10,7 +12,34 @@ const UserSchema = new Schema({
   password: String
 });
 
+// I've looked this up: https://stackoverflow.com/questions/41243575/mongoose-return-error-in-pre-middleware
+// This is a pre save hook on the UserSchema
+UserSchema.pre("save", function(next) {
+  const self = this;
+  // Use mongooses' models to get a connection to the Users table
+  // Search of the given email address already exists in the DB
+  mongoose.models.User.findOne({emailAddress: self.emailAddress}, (err, user) => {
+    // if error throw 500 error
+    if(err) {
+      return next(500);
+    }
+
+    // If a user was found throw an error
+    // because the email address does exist
+    if (user) {
+      const error = new Error('email address must be unique');
+      next(error);
+    } else {
+      // else continue
+      next();
+    }
+  })
+})
+
+// Define courses schema
 const CourseSchema = new Schema({
+  // It references a user ID
+  // which should be the creator/owner of the course
   user: [{type: Schema.Types.ObjectId, ref: 'User'}],
   title: String,
   description: String,
@@ -18,9 +47,11 @@ const CourseSchema = new Schema({
   materialsNeeded: String
 })
 
+// instantiate the models
 const User = mongoose.model("User", UserSchema);
 const Course = mongoose.model("Course", CourseSchema);
 
+// exports the models so they can be used elsewhere
 module.exports = {
     User,
     Course
--- routes.js
+++ routes.js
@@ -9,24 +9,30 @@ const auth = require('basic-auth');
 const User = require("./models").User;
 const Course = require("./models").Course;
 
+// User authentication function
 const authenticateUser = (req, res, next) => {
+    // get the credentials form the request object
     const credentials = auth(req);
 
     if (credentials) {
-        // console.log(credentials.name);
+        
+        // find the user by email address (should be unique)
         User.findOne({'emailAddress': credentials.name}, (error, user) => {
             if (error) {
-                console.warn('error in DB search');
+                console.warn('error in DB search', error);
                 res.status(401).json({message: 'Access Denied'})
             }
 
             if (user) {
                 // We found a user with this email address.
+                // So use bcrypt to compare the hashed password with the given password
                 const authenticated = bcrypt.compareSync(credentials.pass, user.password);
 
                 if (!authenticated) {
                     res.status(401).json({message: 'Access Denied'})
                 } else {
+                    // User is authenticated
+                    // So store the currentUsr on the request object
                     req.currentUser = user;
                     next();
                 }
@@ -41,14 +47,21 @@ const authenticateUser = (req, res, next) => {
     }
 }
 
+// This is a router param
+// Created to easely retrieve courses by ID
 router.param("id", (req, res, next, id) => {
     Course.findById(id, (err, doc) => {
-        if(err) return next(err);
+        if(err) {
+            return next(err);
+        }
+        
         if(!doc) {
             err = new Error("Not found");
             err.status = 404;
             return next(err);
         }
+
+        // set the found course on the request object
         req.course = doc;
         return next();
     })
@@ -56,18 +69,27 @@ router.param("id", (req, res, next, id) => {
 
 // GET /users
 // Route for getting current user
+// authenticateUser: user should be authenticated before this middleware executes
 router.get('/users', authenticateUser, (req, res, next) => {
-    User.find({})
-            .sort({createdAt: -1})
-            .exec((err, users) => {
-                if(err) return next(err);
-                res.status(200);
-                res.json(users);
-            });
+    // because when the user is authenticated req.currentUser will exist
+    // the status is 200 and return the current user
+    if (req.currentUser) {
+
+        res.status(200).json(req.currentUser);
+
+    } else {
+
+        // When this happens authentication succeeded, but something went wrong when setting
+        // the currentUser on the req object
+        console.warn('Auth was succesfull, but User is not found on the request object');
+        res.status(404).json({message: 'User not found'});
+
+    }
 });
 
 // POST /users
 // Route for creating users
+// Validate if all required input exists
 router.post('/users', [
     check('firstName').exists().withMessage('Please provide a firstName'),
     check('lastName').exists().withMessage('Please provide a lastName'),
@@ -76,24 +98,29 @@ router.post('/users', [
 ], (req, res, next) => {
     const errors = validationResult(req);
 
+    // If there are errors send the error messages to the user
     if (!errors.isEmpty()) {
         const errorMessages = errors.array().map(error => error.msg);
-        res.status(400);
-        res.json(errorMessages);
+        res.status(400).json(errorMessages);
     } else {
+        // The user data is on the body of the request object
         const data = req.body;
+        // use bcryt to hash the user password
         bcrypt.hash(data.password, saltRounds, function(error, hash) {
+            // if no errors overwrite the password with the hash
+            // and store the user data in the DB
             if (!error) {
                 data.password = hash;
                 const user = new User(data);
                 user.save((err, user) => {
-                    if(err) return next(err);
-                    res.status(201);
-                    res.json(user);
+                    if(err) {
+                        return next(err);
+                    }
+
+                    res.status(201).json(user);
                 })
             } else {
-                res.status(500)
-                res.json(error);
+                res.status(500).json(error);
             }
         })
     }
@@ -102,29 +129,42 @@ router.post('/users', [
 // GET /courses
 // Route for getting a list of courses
 router.get('/courses', (req, res, next) => {
+    // find and respons with all ccourses
     Course.find({})
             .sort({createdAt: -1})
-            .exec((err, users) => {
-                if(err) return next(err);
-                res.status(200);
-                res.json(users);
+            .exec((err, courses) => {
+                if(err) {
+                    return next(err);
+                }
+
+                res.status(200).json(courses);
             });
 });
 
 // GET /courses/:id
 // Route for getting a specific course
 router.get('/courses/:id', (req, res, next) => {
-    res.status(200);
-    res.json(req.course);
+    // find and respond with a specific course
+    // this course is set on the request body bt the param middleware
+    if (req.course) {
+        res.status(200).json(req.course);
+    } else {
+        const error = new Error('Course not found');
+        next(error);
+    }
 });
 
 // POST /courses
 // Route for creating a course
+// Validate if all required input exists
+// Authenticate user
 router.post('/courses', [
-    check('user').exists().withMessage('Tho who is this course connected?'),
+    check('user').exists().withMessage('To who is this course connected?'),
     check('title').exists().withMessage('Please provide a title'),
     check('description').exists().withMessage('Please provide a description')
-], (req, res, next) => {
+], 
+authenticateUser, 
+(req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -133,31 +173,122 @@ router.post('/courses', [
         res.json(errorMessages);
     } else {
         const course = new Course(req.body);
-        course.save((err, course) => {
-            if(err) return next(err);
-            res.status(201);
-            res.json(course);
-        })
+        if (course) {
+            course.save((err, course) => {
+                if(err) {
+                    return next(err);
+                }
+                
+                console.log(course._id);
+                res.location(`/api/courses/${course._id}`);
+                res.sendStatus(201);
+            })
+        } else {
+            const error = new Error('Course information not found');
+            next(error);
+        }
     }
 });
 
 // PUT /courses/:id
 // Route for updating a specific course
-router.put('/courses/:id', (req, res, next) => {
-    req.course.update(req.body, (err, data) => {
-        if(err) next(err);
-        res.status(201);
-        res.json(data);
+router.put('/courses/:id', [
+    check('user').exists().withMessage('Tho who is this course connected?'),
+    check('title').exists().withMessage('Please provide a title'),
+    check('description').exists().withMessage('Please provide a description')
+], 
+authenticateUser, 
+(req, res, next) => {
+    
+    // Object destructuring to get the currentUser from the req object
+    // Got a linter warning on this at work and find it awesome :p    
+    const { currentUser } = req;
+    const ownerIds = req.body.user;
+
+    // A user may only update a course if he/she is the owner
+    // So loop over owner ids
+    // check if the currentUser is the owner
+    // If so update else forbid the user
+
+    // The id is inside an array
+    // instead of using [0] I decided to use a foreach loop
+    // when more users are associated with a course
+    ownerIds.forEach(id => {
+        User.findById(id, (error, user) => {
+            if (error) {
+                return next(error)
+            }
+
+            console.log(currentUser, user);
+            if (user) {
+                // emailAddresses should be unique
+                // So when the currentUser emailaddress does not match with the courses owner
+                // email adddress te currentUser may not delete it
+                if (currentUser.emailAddress !== user.emailAddress) {
+                        
+                    res.sendStatus(403) 
+            
+                } else {
+                
+                    req.course.update(req.body, (err, data) => {
+                        if(err) {
+                            return next(err);
+                        }
+                
+                        res.sendStatus(201)
+                    });
+            
+                }
+            } else {
+                res.status(403).json({message: 'User not found'})
+            }
+        })
     });
 });
 
 // DELETE /courses/:id
 //  Route for deleting a specific course
-router.delete('/courses/:id', (req, res, next) => {
-    req.course.remove((err) => {
-        if(err) return next(err);
-        res.sendStatus(204);
-    })
+router.delete('/courses/:id', authenticateUser, (req, res, next) => {
+
+    const { currentUser } = req;
+    const ownerIds = req.course.user;
+
+    // A user may only update a course if he/she is the owner
+    // So loop over owner ids
+    // check if the currentUser is the owner
+    // If so update else forbid the user
+    ownerIds.forEach(id => {
+        // find a user by id
+        User.findById(id, (error, user) => {
+            if (error) {
+                return next(error)
+            }
+
+            if (user) {
+                // emailAddresses should be unique
+                // So when the currentUser emailaddress does not match with the courses owner
+                // email adddress te currentUser may not delete it
+                if (currentUser.emailAddress !== user.emailAddress) {
+                        
+                    res.sendStatus(403) 
+            
+                } else {
+                    
+                    // remove the course from the DB
+                    req.course.remove((err) => {
+                        if(err) {
+                            return next(err);
+                        }
+                
+                        res.sendStatus(204);
+                    })
+            
+                }
+            } else {
+                res.status(403).json({message: 'User not found'})
+            }
+        })
+    });
 });
 
 module.exports = router;
